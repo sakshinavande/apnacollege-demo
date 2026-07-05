64d83612848990efb82806ccefc7edbd8226e650
Sakshi Navande <sakshinavande93@gmail.com>
2026-07-05 09:03:20 +0530

    Add FirstClass.java

---
+++ FirstClass.java
@@ -0,0 +1,146 @@
+// import java.util.*;
+
+// import java.util.Scanner;
+
+public class FirstClass{
+    public static void main(String args[])
+    {
+        // Output
+
+
+        // System.out.print("Hello World With java \n from Sakshi \n");
+        // use \n to add next line or println()
+        // System.out.println("Hello World !");
+        // System.out.print("Hello World With java");
+        // System.out.println();
+
+        // System.out.println("*");
+        // System.out.println("**");
+        // System.out.println("***");
+        // System.out.println("****");
+
+        // System.out.println("*\n**" );
+
+     
+        // Variables
+
+        // String Name = "Sakshi";
+        // int age = 19;
+        // double price = 25.36;
+        
+
+    // int a = 10;
+    // int b = 25;
+    // int sum = a + b;
+    // System.out.println(sum);
+    // int diff = b - a ;
+    // System.out.println(diff);
+    // int mul = a* b;
+    // System.out.println(mul);
+
+    // Input
+// Scanner sc = new Scanner(System.in);
+// String name = sc.next();
+// String name = sc.nextLine();
+// nextInt()
+// nextFloat()
+// System.out.println(name);
+    //    int a = sc.nextInt();
+    //    int b = sc.nextInt();
+    //    int sum = a + b;
+    //    System.out.println(sum);
+       
+
+// Homework Problems
+// 1.Try to declare meaningful variables of each type. Eg - a variable named age should be a numeric type (int or float) not byte.
+
+
+        // //Integer types
+        // byte ageInMonth = 19;
+        // short year = 2026;
+        // int age = 19;
+        // long population = 14000000000000L;
+
+        // //Decimal types 
+        // float height = 5.7f;
+        // double salary = 55000.79;
+
+        // //Character type
+        // char grade = 'A';
+
+        // //Boolean type 
+        // boolean isStudent = true;
+
+        // //Non-primitive types
+        // String name = "Sakshi";
+
+        // //Display values
+        // System.out.println("Age in Month:"+ ageInMonth);
+        // System.out.println("Year:"+ year);
+        // System.out.println("Age:"+age);
+        // System.out.println("Population:"+population);
+        // System.out.println("Height:"+height);
+        // System.out.println("Salary:"+salary);
+        // System.out.println("Grade:"+grade);
+        // System.out.println("Is Student:"+isStudent);
+        // System.out.println("Name:"+name);
+
+
+
+        //2. Make a program that takes the radius of a circle as input, calculates its radius and area and prints it as output to the user.
+
+//         Scanner sc = new Scanner(System.in);
+        
+//         // Input radius
+//         System.out.println("Enter the radius of the circle:");
+//         double radius = sc.nextDouble();
+
+//         // Calculate diameter 
+//         double diameter = 2 * radius;
+//         double area = Math.PI * radius * radius;
+
+//         // Display results
+//         System.out.println("Radius:"+radius);
+//         System.out.println("Diameter:"+diameter);
+//         System.out.println("Area:"+area);
+// sc.close();
+
+
+
+// 3.Make a program that prints the table of a number that is input by the user.
+
+// Scanner sc = new Scanner(System.in);
+
+// System.out.println("Enter a Number:");
+// int n = sc.nextInt();
+
+// System.out.println(n*1);
+// System.out.println(n*2);
+// System.out.println(n*3);
+// System.out.println(n*4);
+// System.out.println(n*5);
+// System.out.println(n*6);
+// System.out.println(n*7);
+// System.out.println(n*8);
+// System.out.println(n*9);
+// System.out.println(n*10);
+
+//OR
+
+// for(int i=1; i<=10;i++){
+//     System.out.println(n*i);
+// }
+
+
+// Area of Square
+// Scanner sc = new Scanner(System.in);
+
+// System.out.println("Enter side of the square:");
+// double side = sc.nextDouble();
+
+// double area = side * side;
+//  System.out.println("Area of square :"+ area);
+
+//  sc.close();
+    }
+}
